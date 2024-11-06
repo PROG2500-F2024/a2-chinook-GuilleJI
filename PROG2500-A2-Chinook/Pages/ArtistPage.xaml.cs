570c120d7598fb2722c6bc2152c617c458d8d7aa
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-11-06 10:27:20 -0400

    Add artist list to ArtistPage with EF Core data binding
    Updated ArtistPage.xaml to include a CollectionViewSource for data binding to a ListView. Modified StackPanel to display "Chinook Artist List" and added ListView for artist details. Added necessary using directives in ArtistPage.xaml.cs for EF Core. Instantiated ChinookContext and CollectionViewSource in ArtistPage class, and loaded artist data in the constructor.

--- PROG2500-A2-Chinook/Pages/ArtistPage.xaml.cs
+++ PROG2500-A2-Chinook/Pages/ArtistPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using PROG2500_A2_Chinook.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,19 @@ namespace PROG2500_A2_Chinook.Pages
     /// </summary>
     public partial class ArtistPage : Page
     {
+        ChinookContext context = new ChinookContext();
+        CollectionViewSource artistViewSource = new CollectionViewSource();
         public ArtistPage()
         {
             InitializeComponent();
+            //Tie the markup viewsource object to the C# code viewsource object
+            artistViewSource = (CollectionViewSource)FindResource(nameof(artistViewSource));
+
+            //Use the dbContext to tell EntityFramework to load the data to use on this page
+            context.Artists.Load();
+
+            //Set the viewsource data source to use the artist data collection (dbset)
+            artistViewSource.Source = context.Artists.Local.ToObservableCollection();
         }
     }
 }
