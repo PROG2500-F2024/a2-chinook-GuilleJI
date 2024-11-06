a379bdd256bcd41056c862bfba60b2020a8e6d81
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-11-06 09:43:08 -0400

    Enhance TracksPage layout and add global styles
    Added global and specific styles for TextBlock elements in App.xaml.
    Enhanced TracksPage layout with Expander, ScrollViewer, and ListView.
    Added resources and styles to TracksPage.xaml.
    Updated TracksPage.xaml.cs with necessary using directives.
    Created and linked CollectionViewSource and ChinookContext.
    Loaded track data using Entity Framework.

--- PROG2500-A2-Chinook/Pages/TracksPage.xaml.cs
+++ PROG2500-A2-Chinook/Pages/TracksPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using PROG2500_A2_Chinook.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,24 @@ namespace PROG2500_A2_Chinook.Pages
     /// </summary>
     public partial class TracksPage : Page
     {
+        //Create a new instance of the ChinookContext class 
+        ChinookContext context = new ChinookContext();
+
+        // Create a new instance of the CollectionViewSource class
+        CollectionViewSource trackViewSource = new CollectionViewSource();
         public TracksPage()
         {
             InitializeComponent();
+
+            //Tie the markup viewsource object to the C# code viewsource object 
+            trackViewSource = (CollectionViewSource)FindResource(nameof(trackViewSource));
+
+            //Use the dbContext to tell EntityFramework to load the data to use on this page 
+            context.Tracks.Load();
+
+            //Set the viewsource data source to use the tracks data collection (dbset) 
+            trackViewSource.Source = context.Tracks.Local.ToObservableCollection();
+
         }
     }
 }
