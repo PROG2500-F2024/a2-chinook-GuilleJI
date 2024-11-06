ba3ba643f306e2ad869352513f3b305a188eda9f
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-11-06 10:17:03 -0400

    Add genre list to GenrePage with data binding
    Updated GenrePage.xaml to include a CollectionViewSource resource named genreViewSource for data binding to a ListView control. Changed TextBlock text to "Chinook Genre List". Added ListView with GridView columns for GenreId and Name. In GenrePage.xaml.cs, added using directives for Microsoft.EntityFrameworkCore and PROG2500_A2_Chinook.Data. Introduced ChinookContext and CollectionViewSource fields in GenrePage class. In the constructor, tied genreViewSource to markup, loaded context.Genres data, and set genreViewSource.Source to the local observable collection of genres.

--- PROG2500-A2-Chinook/Pages/GenrePage.xaml.cs
+++ PROG2500-A2-Chinook/Pages/GenrePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using PROG2500_A2_Chinook.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,20 @@ namespace PROG2500_A2_Chinook.Pages
     /// </summary>
     public partial class GenrePage : Page
     {
+        ChinookContext context = new ChinookContext();
+        CollectionViewSource genreViewSource = new CollectionViewSource();
         public GenrePage()
         {
             InitializeComponent();
+
+            //Tie the markup viewsource object to the C# code viewsource object
+            genreViewSource = (CollectionViewSource)FindResource(nameof(genreViewSource));
+
+            //Use the dbContext to tell EntityFramework to load the data to use on this page
+            context.Genres.Load();
+
+            //Set the viewsource data source to use the genre data collection (dbset)
+            genreViewSource.Source = context.Genres.Local.ToObservableCollection();
         }
     }
 }
